c48cd5c54f946dd30eb7d4e1826ec20997cab33a
GOLDER303 <szymonstepien@interia.pl>
2022-12-31 12:04:38 +0100

    Initial commit

---
+++ src/main/java/com/secjar/secjarapi/SecjarApiApplication.java
@@ -0,0 +1,13 @@
+package com.secjar.secjarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecjarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecjarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secjar/secjarapi/SecjarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secjar.secjarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecjarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
