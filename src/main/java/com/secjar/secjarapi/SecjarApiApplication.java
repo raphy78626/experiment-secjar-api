8c4833542f45d289a37337ef710b1913d20f4524
GOLDER303 <szymonstepien@interia.pl>
2023-01-10 15:16:52 +0100

    Add property sources

--- src/main/java/com/secjar/secjarapi/SecjarApiApplication.java
+++ src/main/java/com/secjar/secjarapi/SecjarApiApplication.java
@@ -1,13 +1,18 @@
 package com.secjar.secjarapi;
 
+import com.secjar.secjarapi.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
+@PropertySource("classpath:emailSender.properties")
 @SpringBootApplication
 public class SecjarApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecjarApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecjarApiApplication.class, args);
+    }
 
 }
