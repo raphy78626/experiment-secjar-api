22dda4820ef454ea75f100b5b1d292232a71f724
GOLDER303 <szymonstepien@interia.pl>
2023-02-08 13:18:15 +0100

    Fix wrongly named error messages

--- src/main/java/com/secjar/secjarapi/dtos/requests/UserPatchRequestDTO.java
+++ src/main/java/com/secjar/secjarapi/dtos/requests/UserPatchRequestDTO.java
@@ -7,19 +7,19 @@ public record UserPatchRequestDTO(Long fileDeletionDelay, Long desiredSessionTim
     public UserPatchRequestDTO {
         if (fileDeletionDelay != null) {
             if (fileDeletionDelay < MIN_FILE_DELETION_DELAY) {
-                throw new IllegalArgumentException(String.format("FileDeletionDelay cannot be smaller than %d ms", MIN_FILE_DELETION_DELAY));
+                throw new IllegalArgumentException(String.format("File deletion delay cannot be smaller than %d ms", MIN_FILE_DELETION_DELAY));
             }
             if (fileDeletionDelay > MAX_FILE_DELETION_DELAY) {
-                throw new IllegalArgumentException(String.format("FileDeletionDelay cannot be larger than %d ms", MAX_FILE_DELETION_DELAY));
+                throw new IllegalArgumentException(String.format("File deletion delay cannot be larger than %d ms", MAX_FILE_DELETION_DELAY));
             }
         }
 
         if(desiredSessionTime != null) {
             if(desiredSessionTime < MIN_SESSION_TIME) {
-                throw new IllegalArgumentException(String.format("FileDeletionDelay cannot be smaller than %d ms", MIN_SESSION_TIME));
+                throw new IllegalArgumentException(String.format("Desired session time cannot be smaller than %d ms", MIN_SESSION_TIME));
             }
             if(desiredSessionTime > MAX_SESSION_TIME) {
-                throw new IllegalArgumentException(String.format("FileDeletionDelay cannot be larger than %d ms", MAX_SESSION_TIME));
+                throw new IllegalArgumentException(String.format("Desired session time cannot be larger than %d ms", MAX_SESSION_TIME));
             }
         }
     }
