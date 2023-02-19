a4ca11d253944d9af095e61d2081d1ec40da5d40
GOLDER303 <szymonstepien@interia.pl>
2023-02-19 17:21:07 +0100

    Handle bad credentials exception

--- src/main/java/com/secjar/secjarapi/controllers/ErrorHandlingController.java
+++ src/main/java/com/secjar/secjarapi/controllers/ErrorHandlingController.java
@@ -3,6 +3,7 @@ package com.secjar.secjarapi.controllers;
 import com.secjar.secjarapi.exceptions.InternalException;
 import com.secjar.secjarapi.exceptions.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -16,20 +17,26 @@ public class ErrorHandlingController {
 
     @ExceptionHandler({ResourceNotFoundException.class})
     protected ResponseEntity<Object> handleResourceNotFoundException(Exception ex, WebRequest request) {
-        Map<String, Object> errorMessage = createExceptionResponseBody(ex, 404);
+        Map<String, Object> errorMessage = createExceptionResponseBody(ex.getMessage(), 404);
 
         return ResponseEntity.status(404).body(errorMessage);
     }
 
     @ExceptionHandler({InternalException.class})
     protected ResponseEntity<Object> handleInternalException(Exception ex, WebRequest request) {
-        Map<String, Object> errorMessage = createExceptionResponseBody(ex, 500);
-        return ResponseEntity.status((int)errorMessage.get("status")).body(errorMessage);
+        Map<String, Object> errorMessage = createExceptionResponseBody(ex.getMessage(), 500);
+        return ResponseEntity.status(500).body(errorMessage);
     }
 
-    private static Map<String, Object> createExceptionResponseBody(Exception originalException, int statusCode) {
+    @ExceptionHandler({BadCredentialsException.class})
+    protected ResponseEntity<Object> handleBadCredentialsException(Exception ex, WebRequest request) {
+        Map<String, Object> errorMessage = createExceptionResponseBody("Wrong username, password or 2FA code", 400);
+        return ResponseEntity.status(400).body(errorMessage);
+    }
+
+    private static Map<String, Object> createExceptionResponseBody(String message, int statusCode) {
         Map<String, Object> errorMessage = new HashMap<>();
-        errorMessage.put("error", originalException.getMessage());
+        errorMessage.put("error", message);
         errorMessage.put("status", statusCode);
         errorMessage.put("time_stamp", LocalDateTime.now());
         return errorMessage;
