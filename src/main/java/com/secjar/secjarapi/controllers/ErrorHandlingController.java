84f3c4c7312d7f11aacb65dd1fe425de5be2082d
GOLDER303 <szymonstepien@interia.pl>
2023-03-14 13:24:27 +0100

    Add printing stack trace in error handling controller

--- src/main/java/com/secjar/secjarapi/controllers/ErrorHandlingController.java
+++ src/main/java/com/secjar/secjarapi/controllers/ErrorHandlingController.java
@@ -18,37 +18,42 @@ public class ErrorHandlingController {
     @ExceptionHandler({ResourceNotFoundException.class})
     protected ResponseEntity<Object> handleResourceNotFoundException(Exception ex, WebRequest request) {
         Map<String, Object> errorMessage = createExceptionResponseBody(ex.getMessage(), 404);
-
+        ex.printStackTrace();
         return ResponseEntity.status(404).body(errorMessage);
     }
 
     @ExceptionHandler({InternalException.class})
     protected ResponseEntity<Object> handleInternalException(Exception ex, WebRequest request) {
         Map<String, Object> errorMessage = createExceptionResponseBody(ex.getMessage(), 500);
+        ex.printStackTrace();
         return ResponseEntity.status(500).body(errorMessage);
     }
 
     @ExceptionHandler({BadCredentialsException.class, UsernameNotFoundException.class})
     protected ResponseEntity<Object> handleBadCredentialsException(Exception ex, WebRequest request) {
         Map<String, Object> errorMessage = createExceptionResponseBody("Wrong username, password or 2FA code", 401);
+        ex.printStackTrace();
         return ResponseEntity.status(401).body(errorMessage);
     }
 
     @ExceptionHandler({EmailNotVerifiedException.class})
     protected ResponseEntity<Object> handleEmailNotVerifiedException(Exception ex, WebRequest request) {
         Map<String, Object> errorMessage = createExceptionResponseBody(ex.getMessage(), 401);
+        ex.printStackTrace();
         return ResponseEntity.status(401).body(errorMessage);
     }
 
     @ExceptionHandler({BadNewPasswordException.class})
     protected ResponseEntity<Object> handleBadNewPasswordException(Exception ex, WebRequest request) {
         Map<String, Object> errorMessage = createExceptionResponseBody("Bad password. Password need to have between 8 and 30 letters, at least one uppercase letter, one lowercase letter, one digit and one special character.", 400);
+        ex.printStackTrace();
         return ResponseEntity.status(400).body(errorMessage);
     }
 
     @ExceptionHandler({BadEmailException.class})
     protected ResponseEntity<Object> handleBadEmailException(Exception ex, WebRequest request) {
         Map<String, Object> errorMessage = createExceptionResponseBody(ex.getMessage(), 400);
+        ex.printStackTrace();
         return ResponseEntity.status(400).body(errorMessage);
     }
 
