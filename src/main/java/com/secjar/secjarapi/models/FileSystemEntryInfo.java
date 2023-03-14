31bba0ce8c1c0c4b3df89b0f56d48c95e1fe064a
GOLDER303 <szymonstepien@interia.pl>
2023-03-14 18:04:58 +0100

    Add upload date to file system entry info

--- src/main/java/com/secjar/secjarapi/models/FileSystemEntryInfo.java
+++ src/main/java/com/secjar/secjarapi/models/FileSystemEntryInfo.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
